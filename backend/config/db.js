10597ffc9719e6342b644d12df28c8c26aa4e6eb
Ozi <ozisadiqkasai@gmail.com>
2025-07-09 11:50:36 +0100

    added prod and dev env to config

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,21 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+
+// Dynamically load the correct .env file
+if (process.env.NODE_ENV === 'production') {
+  dotenv.config({ path: '.env.production' });
+} else {
+  dotenv.config(); // defaults to .env
+}
 
 const connectDB = async () => {
-    try {
-        const connect = await mongoose.connect(process.env.production.MONGO_URI)
-        console.log(`MongoDB connected: ${connect.connection.host}`)
-    } catch (error) {
-        console.error('MongoDB connection error:', error.message)
-        process.exit(1)
-    }
-}
+  try {
+    const connect = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${connect.connection.host}`);
+  } catch (error) {
+    console.error('MongoDB connection error:', error.message);
+    process.exit(1);
+  }
+};
 
 export default connectDB;
